62578eb0a698b256bc795ba077caf287ef7e1e36
Kartikeya <kartikeya9917@gmail.com>
2026-08-09 15:48:01 +0530

    fix: use setUTCHours/setUTCDate to prevent server-client timezone mismatch

--- Part3/Session-Reschedule-Widget/lib/mockData.ts
+++ Part3/Session-Reschedule-Widget/lib/mockData.ts
@@ -2,8 +2,8 @@ import { Session } from './types';
 
 function getUpcomingDate(daysFromNow: number, hour: number, minute: number = 0) {
   const d = new Date();
-  d.setDate(d.getDate() + daysFromNow);
-  d.setHours(hour, minute, 0, 0);
+  d.setUTCDate(d.getUTCDate() + daysFromNow);
+  d.setUTCHours(hour, minute, 0, 0);
   return d.toISOString();
 }
 
